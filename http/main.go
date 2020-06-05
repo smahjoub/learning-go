8937eadd73e77d0252a73121ec166e2b19ed57aa
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2020-06-05 20:03:15 +0200

    create custom writer

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 
@@ -15,7 +18,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Printf(string(bs))
+
+	return len(bs), nil
 }
