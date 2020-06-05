2d1b2e1ce1e5c9cc8ddbce4975ea97bf06269573
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2020-06-05 12:48:53 +0200

    Embedding Structs

--- structs/main.go
+++ structs/main.go
@@ -2,21 +2,37 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode string
+}
+
 type person struct {
-	firstName string
-	lastName  string
+	firstName   string
+	lastName    string
+	contactInfo contactInfo
 }
 
 func main() {
 	// p := person{"Saif Eddine", "Mahjoub"}
 
-	p := person{firstName: "Saif Eddine", lastName: "Mahjoub"}
+	p := person{
+		firstName: "Saif Eddine",
+		lastName:  "Mahjoub",
+		contactInfo: contactInfo{
+			email:   "se.mahjoub@mail.com",
+			zipCode: "75000",
+		},
+	}
 	fmt.Println(p)
 
 	var p2 person
 
 	p2.firstName = "Alex"
 	p2.lastName = "Andersson"
-
+	p2.contactInfo = contactInfo{
+		email:   "se.mahjoub@mail.com",
+		zipCode: "75000",
+	}
 	fmt.Printf("%+v", p2)
 }
