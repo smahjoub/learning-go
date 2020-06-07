d8e54818166cf70d108e0324a35058cb9767e476
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2020-06-07 21:25:26 +0200

    add endless website checkeer

---
+++ channels/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://www.google.com",
+		"http://www.facebook.com",
+		"http://www.stackoverflow.com",
+		"http://www.amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for {
+		go checkLink(<-c, c)
+	}
+
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down.")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- link
+}
